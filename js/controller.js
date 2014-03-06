455df6111035ca23a7a309668384313019c306b2
ysobj <litailang.ysobj@gmail.com>
2014-03-06 18:10:07 +0900

    20140306

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,19 @@
 var lifeplanApp = angular.module('lifeplanApp', []);
 
 lifeplanApp.controller('lifeplanCtrl', function($scope){
+    $scope.append = function(){
+      var maxYear = 0;
+      angular.forEach($scope.datas, function(value){
+        if(maxYear < value.year){
+          maxYear = value.year;
+        }
+      });
+      $scope.datas.push(
+        {
+          'year' : maxYear+1,
+          'deposit' : Math.floor( Math.random() * 300 )
+        });
+    };
     $scope.datas = [
     {
       'year' : 2013,
