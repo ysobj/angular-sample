4c50db3707aa63e5150ab0cf3b93c6351e101a85
ysobj <litailang.ysobj@gmail.com>
2014-03-07 16:13:27 +0900

    20140307

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,26 @@
 var lifeplanApp = angular.module('lifeplanApp', []);
 
 lifeplanApp.controller('lifeplanCtrl', function($scope){
+    $scope.ageAtStart = 22;
+    $scope.depositPerYear = 1000000;
+    $scope.interestRate = 101;
+    $scope.refresh = function(){
+      $scope.datas = [];
+      var totalDeposit = 0;
+      var age = parseInt($scope.ageAtStart, 10);
+      var remmaining = 60 - age;
+      var end = 2014 + remmaining;
+      for(var i = 2014; i <= end; i++){
+        $scope.datas.push(
+          {
+            'year' : i,
+            'age' : age,
+            'deposit' : totalDeposit
+          });
+        totalDeposit = (totalDeposit * parseInt($scope.interestRate, 10)) / 100  + parseInt($scope.depositPerYear, 10);
+        age++;
+      }
+    };
     $scope.append = function(){
       var maxYear = 0;
       angular.forEach($scope.datas, function(value){
@@ -11,7 +31,7 @@ lifeplanApp.controller('lifeplanCtrl', function($scope){
       $scope.datas.push(
         {
           'year' : maxYear+1,
-          'deposit' : Math.floor( Math.random() * 300 )
+          'deposit' : $scope.depositPerYear
         });
     };
     $scope.datas = [
